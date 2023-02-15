bfd60805ff5da0cece0fba842b2d322e6544cc36
Shiyal Pradip ✌ <98959751+ShiyalPradip@users.noreply.github.com>
2023-02-15 06:12:35 +0530

    Add files via upload

---
+++ Phantom/index.php
@@ -0,0 +1,34 @@
+<?php
+    include('LinkCSS.php');
+    echo("<title> PHP Assignments</title>");
+?>
+ 
+    <main>
+        <section>
+            <h3> Assignments </h3>
+            <div class="BgColor">
+                <table class="table table-bordered">
+                    <tr align="center">
+                        <td> <img src="Images/Assignment.png" class="PhpMargin"/> <a href="Assignment1.php" class="Links"> Assignment1 </a> </td>
+                        <td> <img src="Images/Assignment.png" class="PhpMargin"/> <a href="Assignment2.php" class="Links"> Assignment2 </a> </td>
+                    </tr>
+                </table>
+            </div>
+        </section>
+
+        <section>
+            <h3> My Programs </h3>
+            <div class="BgColor">
+                <table class="table table-bordered">
+                    <tr> <td> <img src="Images/Hacker.png" class="Prg-Image"> <a href="Programs/Calculator.php" target="_blank" class="Links"> Program to Make a Mathematical Calculator In PHP  </a> </td> </tr>
+                    <tr> <td> <img src="Images/Hacker.png" class="Prg-Image"> <a href="Programs/Table.php"      target="_blank" class="Links"> Program To Print Multiplication Table Of a Number </a> </td> </tr>
+                    <tr> <td> <img src="Images/Hacker.png" class="Prg-Image"> <a href="Programs/Fibonacci.php"  target="_blank" class="Links"> Program To Print Fibonacci Series Of a Number     </a> </td> </tr>
+                    <tr> <td> <img src="Images/Hacker.png" class="Prg-Image"> <a href="Programs/Factorial.php"  target="_blank" class="Links"> Program To Calculate Factorial Of a Number        </a> </td> </tr>
+                    <tr> <td> <img src="Images/Hacker.png" class="Prg-Image"> <a href="Programs/Ascii.php"      target="_blank" class="Links"> Convert ASCII Value To Character                  </a> </td> </tr>
+                    <tr> <td> <img src="Images/Hacker.png" class="Prg-Image"> <a href="Programs/Ascii2.php"     target="_blank" class="Links"> Programe To Print Square Of a Number              </a> </td> </tr>
+                    <tr> <td> <img src="Images/Hacker.png" class="Prg-Image"> <a href="Programs/Pascal.php"     target="_blank" class="Links"> Programe To Print The Pascal Triangle             </a> </td> </tr>
+                </table>
+            </div>
+    </section>
+    </main>
+</body>
