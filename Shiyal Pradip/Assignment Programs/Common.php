b32d3a2c511e2674ebad03bb00c45d045eb1895a
Shiyal Pradip <98959751+ShiyalPradip@users.noreply.github.com>
2024-06-28 14:08:33 +0530

    Delete Shiyal Pradip directory

--- Shiyal Pradip/Assignment Programs/Common.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo("
-        <link rel='stylesheet' href='CSS/bootstrap.min.css'/>
-        <link rel='stylesheet' href='CSS/Common.css'/>
-        <link rel='icon' type='image/x-icon' href='Channel Logo.png'/>
-    ");
-?>
--- Shiyal Pradip/Footer.php
+++
@@ -1,9 +0,0 @@
-                <div class="row BgColor">
-                    <div class="col">
-                        <table class="FooterTable" height="280px" width="100%">
-                            <tr> <td align="center"> <img src="Images/Channel Logo.png" alt=""/> </td> </tr>
-                            <tr> <td align="center"> <h2> Copyright &copy; 2022 Pro Programmer</h2> </td> </tr>
-                            <tr> <td> <marquee behavior="alternate" direction=""> Made By Shiyal Pradip </marquee> </td> </tr>
-                        </table>
-                    </div>
-                </div>
